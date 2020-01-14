2e5866ad4519373040be786f126395b7352941c6
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 16:34:24 +0200

    Add project files.

---
+++ 29.uzd int array break when 0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _29.uzd_int_array_break_when_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 1, 1, 0, 1, 1, 1, 1 };
+
+            int n = 0;
+
+            do
+            {
+                if (numbers[n] == 0)
+                {
+                    Console.WriteLine($"{numbers[n]} is find in position {n}");
+                    break;
+                }
+                n++;
+                continue;
+            }
+            while (true);
+        }
+    }
+}
+// Izveidot int masīvu ar vērtībām 1,1,1,0,1,1,1, 1. Programmai ir jāpārtrauc darbs, kad atrodam 0. Izmantojam break;
